c990bf93103160b657a9450bdaa17e8b97a62951
dustyshap <code@getbraintree.com>
2018-05-25 20:51:12 +0000

    Add permutations

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
+from itertools import permutations
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/permute", methods=["POST"])
+def permute():
+    text_input = request.form['text']
+    return render_template('permute.html', permutations=permutations(text_input))
